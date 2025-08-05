7ecb081a47dc8ebf14f3054734c85a0286f07f4f
Emiliahrabeson <emiliahrabeson@gmail.com>
2025-08-05 12:53:35 +0300

    + ou - fini xD

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 let cartes = ["A", "A", "B", "B", "C", "C", "D", "D", "E", "E", "F", "F"];
-
 function melanger(tab) {
     for (let i = tab.length-1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
@@ -11,6 +10,7 @@ function melanger(tab) {
 function create_card() {
     const cardTable = document.getElementById("card_table");
     const tab_melangee = melanger(cartes);
+    cardTable.innerHTML = "";
 
     tab_melangee.forEach(lettre => {
         const card = document.createElement('div');
@@ -28,23 +28,73 @@ function create_card() {
         cardTable.appendChild(card);
     });
 
+}
+
+function play() {
+    let compteur = 0;
+    let compteur_score = 0;
+    let cartesCliquees = [];
+    let congrats = document.querySelector('.fin'); 
+
+    if (congrats) {
+        congrats.remove();
+    }
+
+    const bouton = document.querySelector('.Play_game'); 
+    bouton.innerHTML = "Replay";
+
+    create_card();
+
     setTimeout(() => {
         const cards = document.querySelectorAll('.card');
         cards.forEach(card => {
             card.classList.add('flipped');
+            
         });
-    }, 5000); 
+    }, 3000); 
 
     const cards = document.querySelectorAll('.card');
     cards.forEach(carte => {
         carte.addEventListener("click", () => {
-            carte.classList.toggle('flipped');      //suppr ou ajoute flipped
+           if (compteur >= 2 || cartesCliquees.includes(carte)) {
+                return;
+            }
+            if (carte.classList.contains('flipped')) {
+                carte.classList.toggle('flipped');
+                cartesCliquees.push(carte);
+                compteur++;
+            }
+
+            if (compteur === 2) {
+                const lettre1 = cartesCliquees[0].querySelector('.face').textContent;
+                const lettre2 = cartesCliquees[1].querySelector('.face').textContent;
+
+                if (lettre1 === lettre2) {
+                    compteur_score++;
+                    cartesCliquees = []; 
+                    compteur = 0;
+
+                    if (compteur_score === 6) {
+                        const congrats = document.createElement('div');
+                        congrats.textContent = "Congratulation";
+                        congrats.className = "fin";
+                        document.body.appendChild(congrats); 
+                    }
+                }
+                else {
+                    setTimeout(() => {
+                        cartesCliquees[0].classList.toggle('flipped');
+                        cartesCliquees[1].classList.toggle('flipped');
+                        cartesCliquees = [];
+                        compteur = 0;
+                    }, 1000);
+                }
+            }
+
+
         });
-    });
     
+            
+    });
 
 }
-
-
-
-create_card();
